2ed661046bef9106237dbbc16fbbbf776b76c20c
alejandro.orellano <aleback.2021@gmail.com>
2021-12-13 22:33:44 -0300

    Iniciando repo

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//const edad=18;
+
+console.log("Prueba desde archivo js");
+var nombre =prompt("Ingresar Nombre");
+let resultadoNombre= alert("Hola "+nombre+" ahora necesitamos tu edad.");
+var edad =prompt("Ingresar Edad");
+var resEdad=edad;
+var resultadoEdad;
+ if (resEdad<18) {
+    resultadoEdad=alert("No es posible que accedas");
+}else {
+
+    resultadoEdad=alert("Eres mayor puedes continuar");
+}
