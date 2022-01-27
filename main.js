f1f7438d473f861ca1924f3728f86294ef693f4d
alejandro.orellano <aleback.2021@gmail.com>
2022-01-26 21:39:08 -0300

    desafio storage

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ class producto {
     }
 }
 let totalart=0;
+let contadorProd;
 function calcularPrecio(resultadoNombr,precio,iva,cantProd){
     
     
@@ -22,10 +23,6 @@ function calcularPrecio(resultadoNombr,precio,iva,cantProd){
     precio=document.getElementById("precio");
     cantProd=document.getElementById("cantidad");
     iva=document.getElementById("iva");
-    /*cantProd=parseInt(cantProd);
-    resultadoNombr=prompt("Ingresar nombre");
-    cantProd=prompt("Ingresar cantidad de productos");
-    alert("Hola "+resultadoNombr+" vas a ingresar "+cantProd+" productos");*/
     
 
         let product= new producto(resultadoNombr.value,precio.value,iva.value,cantProd.value);
@@ -34,19 +31,16 @@ function calcularPrecio(resultadoNombr,precio,iva,cantProd){
         iva=parseInt(product.iva);
         precio=parseInt(product.precio);        
         precio=precio+(precio*(iva/100));
-    
+        let cant=parseInt(cantProd.value)
         alert("El producto "+product.nomProd+" tiene un precio con iva de: "+precio+" y sin iva de: "+product.precio);
-        let totPr=parseInt(precio*can);
+        let totPr=parseInt(precio)*(cant);
         product.precioTot=totPr;       
-        
+        localStorage.setItem("productos",cantProd);
         valor.push(product);
-        
+        localStorage.setItem("prodCompleto",JSON.stringify(product));       
     
-    let cliente = document.createElement("h3")
-   // cliente.innerHTML="Cliente:"+resultadoNombr;
-   // document.getElementById("client").appendChild(cliente);
-
 }
+
 function totalProductos(){    
    
     for(prod of valor){
@@ -55,36 +49,29 @@ function totalProductos(){
         contenedor.innerHTML = `<h3> Producto: ${prod.nomProd}</h3>
                             <p> Precio: $ ${prod.precio} Iva: % ${prod.iva}</p>
                             <b>Precio c/ iva: $ ${prod.precioTot}</b>
-                            <hr>`;
-    
+                            <hr>`;    
         document.getElementById("resultado").appendChild(contenedor);
-        console.log("Nombre producto: " +prod.nomProd+ " con iva: " + prod.iva + "% .Precio unitario: $" +prod.precio+ " precio total: $" + prod.precioTot);      
+        console.log("Nombre producto: " +prod.nomProd+ " con iva: " + prod.iva + "% .Precio unitario: $" +prod.precio+ " precio total: $" + prod.precioTot);
         
     }
 
     const menoraCien=valor.filter(pre=>pre.precioTot < 100);
     console.log(menoraCien);
-    
-
-   
-    for(pr of valor){
-
-        
+    for(pr of valor){        
          totalart+=pr.precioTot;
-         console.log(pr.precioTot);
-        
+         console.log(pr.precioTot);               
     }
     let total = document.createElement("h4")
         total.innerHTML="Total: "+parseFloat(totalart);
-    document.getElementById("totRt").appendChild(total);
-  
-/*function totalProd(){
-
-}*/
-
+    document.getElementById("totRt").appendChild(total); 
 }
+
 let agregarProd=document.getElementById("btnPrincipal");
 agregarProd.addEventListener("click", calcularPrecio);
 let calcularTotal=document.getElementById("btnTotal");
 calcularTotal.addEventListener("click",totalProductos);
+console.log(localStorage.getItem("prodCompleto"));
+
+
+
 //calcularPrecio();
